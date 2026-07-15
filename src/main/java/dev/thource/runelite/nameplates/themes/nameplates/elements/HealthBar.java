88fff83f917eb3ccb171824e47030511df0c15a6
Thource <me@thource.dev>
2026-07-15 22:44:20 +0100

    #2 - Add poison damage indicator

--- src/main/java/dev/thource/runelite/nameplates/themes/nameplates/elements/HealthBar.java
+++ src/main/java/dev/thource/runelite/nameplates/themes/nameplates/elements/HealthBar.java
@@ -1,6 +1,12 @@
 package dev.thource.runelite.nameplates.themes.nameplates.elements;
 
 import dev.thource.runelite.nameplates.Nameplate;
+import dev.thource.runelite.nameplates.NameplatesPlugin;
+import dev.thource.runelite.nameplates.panel.components.ColorInput;
+import dev.thource.runelite.nameplates.panel.components.LabelledInput;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +19,7 @@ import net.runelite.client.plugins.itemstats.stats.Stats;
 @SuperBuilder
 public class HealthBar extends Bar {
   @Builder.Default HealthBarColorProvider barColorProvider = new HealthBarColorProvider();
+  @Builder.Default protected Color poisonDamageColor = new Color(30, 90, 20);
 
   @Override
   public boolean shouldDraw(Nameplate nameplate) {
@@ -32,5 +39,53 @@ public class HealthBar extends Bar {
   @Override
   protected StatChange getStatChange(Nameplate nameplate) {
     return nameplate.getHoveredItemStatChange(Stats.HITPOINTS);
+  }
+
+  @Override
+  public void draw(Nameplate nameplate, Graphics2D graphics, int plateX, int plateY) {
+    super.draw(nameplate, graphics, plateX, plateY);
+
+    var poisonStatus = nameplate.getPoisonStatus();
+    if (!shouldDraw(nameplate) || poisonStatus == null) {
+      return;
+    }
+
+    // If the player has boosted health and the poison damage won't take them under their max
+    // health, there is nothing to draw
+    var visiblePoisonDamage =
+        poisonStatus.getDamage()
+            - Math.max(0, nameplate.getCurrentHealth() - nameplate.getMaxHealth());
+    if (visiblePoisonDamage <= 0) {
+      return;
+    }
+
+    var x = plateX + xPositionProvider.get(width);
+    var y = plateY + yPositionProvider.get(height);
+    var innerWidth = width - borderSize * 2;
+    var innerHeight = height - borderSize * 2;
+
+    var fillWidth = (int) Math.ceil(innerWidth * getProgress(nameplate));
+    var poisonDamageWidth =
+        (int) Math.ceil(innerWidth * (visiblePoisonDamage / (float) nameplate.getMaxHealth()));
+
+    Rect.draw(
+        graphics,
+        x + borderSize + fillWidth - poisonDamageWidth,
+        y + borderSize,
+        poisonDamageWidth,
+        innerHeight,
+        poisonDamageColor,
+        cornerRadius - borderSize);
+  }
+
+  @Override
+  public List<LabelledInput> getEditInputs(NameplatesPlugin plugin) {
+    var inputs = super.getEditInputs(plugin);
+
+    inputs.add(
+        new ColorInput(
+            "Poison damage color", poisonDamageColor, this::setPoisonDamageColor, plugin));
+
+    return inputs;
   }
 }
