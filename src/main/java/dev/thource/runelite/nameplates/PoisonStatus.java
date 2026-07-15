6610bb6c808058e7e2fe90ddd51043cfcc3ebfd3
Thource <me@thource.dev>
2026-07-15 22:44:20 +0100

    #2 - Add poison damage indicator

--- src/main/java/dev/thource/runelite/nameplates/PoisonStatus.java
+++ src/main/java/dev/thource/runelite/nameplates/PoisonStatus.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PoisonStatus {
   public static final int POISON_TICK_MILLIS = 18_200;
-  private static final int VENOM_MAXIUMUM_DAMAGE = 20;
+  private static final int VENOM_MAXIMUM_DAMAGE = 20;
   private static final int VENOM_THRESHOLD = 1_000_000;
   private final int value;
 
@@ -17,7 +17,7 @@ public class PoisonStatus {
     if (isVenom()) {
       // Venom damage starts at 6, and increments in twos;
       // The VarPlayer increments in values of 1, however.
-      return Math.min(VENOM_MAXIUMUM_DAMAGE, (value - VENOM_THRESHOLD + 3) * 2);
+      return Math.min(VENOM_MAXIMUM_DAMAGE, (value - VENOM_THRESHOLD + 3) * 2);
     }
 
     return (int) Math.ceil(value / 5.0f);
